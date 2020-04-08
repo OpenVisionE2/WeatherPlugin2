180549f2c45ff6de7e6469400484f16d6a8f07f8
persianpros <persianpros@yahoo.com>
2020-04-09 04:02:22 +0430

    Follow correct folder structure for distutils.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 import setup_translate
 
 pkg = 'Extensions.WeatherPlugin2'
-setup (name = 'enigma2-plugin-extensions-WeatherPlugin2',
+setup (name = 'enigma2-plugin-extensions-weatherplugin2',
        version = '2.1',
-       description = 'Plugin to Weather.',
+       description = 'Weather plugin 2',
        packages = [pkg],
-       package_dir = {pkg: 'usr'},
+       package_dir = {pkg: 'plugin'},
        package_data = {pkg: ['plugin.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass = setup_translate.cmdclass, # for translation
       )
