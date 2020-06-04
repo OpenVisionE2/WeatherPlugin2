98c41607f45e5ee97b63691858cc1085d0bc8374
RAED <rrrr53@hotmail.com>
2020-06-04 19:29:48 +0300

    Fix compile weather.png

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup (name = 'enigma2-plugin-extensions-weatherplugin2',
        description = 'Weather plugin 2',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['plugin.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
+       package_data = {pkg: ['weather.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass = setup_translate.cmdclass, # for translation
       )
