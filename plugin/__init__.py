3ee15b289140ee2dc37ed9d683deb02fe672d0d2
persianpros <persianpros@gmail.com>
2020-10-30 11:52:36 +0330

    Use futurize to have python 3 compatible commas

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import print_function
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
-import os,gettext
+import os, gettext
 
 def localeInit():
 	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
